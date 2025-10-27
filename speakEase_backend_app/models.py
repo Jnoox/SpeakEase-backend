9aee5f1443e1fbc56d3b65191b8bb77f63341408
Jana Sandeyouni <Jnoo66@icloud.com>
2025-10-27 20:39:13 +0300

    add TrainingSession model and register it to admin

--- speakEase_backend_app/models.py
+++ speakEase_backend_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+# TrainingSession model
+class TrainingSession(models.Model):
+    TRAINING_TYPES = [
+        ('voice', 'Voice Training'),
+        ('conversation', 'Camera Conversation'),
+    ]
+
+    training_type = models.CharField(max_length=20, choices=TRAINING_TYPES)
+    duration = models.PositiveIntegerField(help_text="Duration in seconds")
+    audio_file = models.FileField(upload_to='audio/', null=True, blank=True)
+    video_file = models.FileField(upload_to='videos/', null=True, blank=True)
+    feedback_text = models.TextField(blank=True)
+
+    # performance metrics
+    repeated_words = models.PositiveIntegerField(default=0)
+    mispronunciations = models.PositiveIntegerField(default=0)
+    facial_feedback_score = models.FloatField(default=0.0)
+    score = models.FloatField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.training_type.capitalize()} Training - {self.created_at.strftime('%Y-%m-%d')}"
+
