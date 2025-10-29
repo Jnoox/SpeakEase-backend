f46bc50d279bb3fb95c316b1852bc4e7e6b21466
Jana Sandeyouni <Jnoo66@icloud.com>
2025-10-29 10:45:42 +0300

    add the serializers for new models

--- speakEase_backend_app/serializers.py
+++ speakEase_backend_app/serializers.py
@@ -2,14 +2,30 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import TrainingSession, ProgressAnalytics, Tip, UserProfile, VocabularyWord,DailyQuestion
 
+User = get_user_model()
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+        
 class TrainingSessionSerializer(serializers.ModelSerializer):
+    # get the username from User model
+    user_username = serializers.CharField(source='user.username', read_only=True)
     class Meta:
         model = TrainingSession
         fields = '__all__'
 
 class ProgressAnalyticsSerializer(serializers.ModelSerializer):
+    # get the username from User model
+    user_username = serializers.CharField(source='user.username', read_only=True)
     class Meta:
         model = ProgressAnalytics
         fields = '__all__'
@@ -19,4 +35,12 @@ class TipSerializer(serializers.ModelSerializer):
         model = Tip
         fields = '__all__'
 
-
+class VocabularyWordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VocabularyWord
+        fields = '__all__'
+        
+class DailyQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyQuestion
+        fields = '__all__'
