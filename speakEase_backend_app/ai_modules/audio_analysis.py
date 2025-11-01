99ae54e93d30ef0baba698eee747b87957fa1a51
Jana Sandeyouni <Jnoo66@icloud.com>
2025-11-01 22:22:31 +0300

    add new venv for import whisper pakages, and test the get_transcription_whisper method

--- speakEase_backend_app/ai_modules/audio_analysis.py
+++ speakEase_backend_app/ai_modules/audio_analysis.py
@@ -5,6 +5,11 @@ import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+import torch
+import librosa
+import torch
+
 # Creating a Recognizer instance
 r = sr.Recognizer()
 
@@ -55,4 +60,53 @@ def get_large_audio_transcription_on_silence(path):
     # return the text for all chunks detected
     return whole_text
 
-print(get_large_audio_transcription_on_silence("speakEase_backend_app/test_audio/record_out.wav"))
+# print(get_large_audio_transcription_on_silence("speakEase_backend_app/test_audio/record_out.wav"))
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# whisper_model_name = "openai/whisper-tiny.en" # English-only, ~ 151 MB
+# whisper_model_name = "openai/whisper-base.en" # English-only, ~ 290 MB
+# whisper_model_name = "openai/whisper-small.en" # English-only, ~ 967 MB
+# whisper_model_name = "openai/whisper-medium.en" # English-only, ~ 3.06 GB
+whisper_model_name = "openai/whisper-small" 
+# whisper_model_name = "openai/whisper-base" # multilingual, ~ 290 MB
+# whisper_model_name = "openai/whisper-small" # multilingual, ~ 967 MB
+# whisper_model_name = "openai/whisper-medium" # multilingual, ~ 3.06 GB
+# whisper_model_name = "openai/whisper-large-v2" # multilingual, ~ 6.17 GB
+
+# load the model and the processor
+whisper_processor = WhisperProcessor.from_pretrained(whisper_model_name)
+whisper_model = WhisperForConditionalGeneration.from_pretrained(whisper_model_name).to(device)
+
+def load_audio(audio_path):
+  """Load the audio file & convert to 16,000 sampling rate"""
+  # load our wav file
+  speech, sr = librosa.load(audio_path, sr=16000)
+  speech = librosa.util.normalize(speech) 
+  return torch.tensor(speech)
+
+def get_transcription_whisper(audio_path, model, processor, language="english", skip_special_tokens=True):
+  # resample from whatever the audio sampling rate to 16000
+  speech = load_audio(audio_path)
+  # get the input features from the audio file
+  input_features = processor(speech, return_tensors="pt", sampling_rate=16000).input_features.to(device)
+  # get the forced decoder ids
+  forced_decoder_ids = processor.get_decoder_prompt_ids(language=language, task="transcribe")
+  # generate the transcription
+  predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
+  # decode the predicted ids
+  transcription = processor.batch_decode(predicted_ids, skip_special_tokens=skip_special_tokens)[0]
+  return transcription
+
+if __name__ == "__main__":
+    english_transcription = get_transcription_whisper("speakEase_backend_app/test_audio/record_out.wav",
+                            whisper_model,
+                            whisper_processor,
+                            language="english",
+                            skip_special_tokens=True)
+    print("English transcription:", english_transcription)
+    arabic_transcription = get_transcription_whisper("speakEase_backend_app/test_audio/record_arabic.wav",
+                          whisper_model,
+                          whisper_processor,
+                          language="arabic",
+                          skip_special_tokens=True)
+    print("Arabic transcription:", arabic_transcription)
